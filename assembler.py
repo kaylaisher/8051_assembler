2ff8c3e7e42a6586331435e2e2988d1f0db33f94
kaylaisher <2005940427h@gmail.com>
2026-03-15 22:59:04 +0800

    a lot of changes

--- assembler.py
+++ assembler.py
@@ -1,7 +1,8 @@
 import re
-from main import MOV, SUBB, XRL, LCALL, INC, RET, JZ, LABEL
+from main import MOV, SUBB, XRL, LCALL, INC, RET, JZ
 from util import tool
 from Address import Data
+from location import Location
 
 output_file_path = "out/Test01-out.txt"
 log_file_path = "log/Test01-log.txt"
@@ -36,22 +37,8 @@ with open("test_file/Test01.txt") as f:
                     
 
                     if(":" in x_split[0]):
-                        # clean_op = tool.check_format(x_split[0])
-                        # log.write("LABEL: " + clean_op + '\n')
-                        # Data.current_addr_cnt = Data.next_addr_cnt
-                        # Data.addr_data.append(Data.addr_detail)
-                        # Data.next_addr_cnt += 0
-                        # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                        
-                        # log.write("addr detail: \n")
-                        # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                        # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                        # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                        # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                        
-
-                        # log.write("-----------------------------\n")
-                        LABEL.label_location(x_split, text_file, log)
+                       
+                        Location.label_location(x_split, text_file, log)
 
 
                     elif(x_split[0] == "MOV"):
@@ -60,215 +47,65 @@ with open("test_file/Test01.txt") as f:
                             #text_file.write(element + " ")
                             #log.write("element: "+element+"\n")
                         if ("H" in x_split[1]) and ("H" in x_split[2]):
-                            # MOV direct, direct --> 10000101src_direct dest_direct
-                            # clean_op = tool.check_format(x_split[0])
-                            # log.write("OPCODE : " + clean_op + '\n')
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += MOVaddr.direct_direct_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
                             
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            
-                            MOV.direct_direct_location(x_split, text_file, log)
+                            Location.MOVdirect_direct_location(x_split, text_file, log)
                             MOV.direct_direct(x_split, text_file, log)
 
                             
                         elif ("@" in x_split[1]) and ("#" in x_split[2]):
-                            # MOV @Ri, #imm --> 0111011i immediate
-                            # clean_op = tool.check_format(x_split[0])
-                            # log.write("OPCODE : " + clean_op + '\n')
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += MOVaddr.reg_imm_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            MOV.reg_imm_location(x_split, text_file, log)
+                            Location.MOVreg_imm_location(x_split, text_file, log)
                             MOV.reg_imm(x_split, text_file, log)
 
                         elif ("R" in x_split[1]) and ("H" in x_split[2]):
-                            #MOV Rn, direct --> 10101nnn direct
-                            # clean_op = tool.check_format(x_split[0])
-                            # log.write("OPCODE : " + clean_op + '\n')
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += MOVaddr.Rn_direct_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            MOV.Rn_direct_location(x_split, text_file, log)
+                            Location.MOVRn_direct_location(x_split, text_file, log)
                             MOV.Rn_direct(x_split, text_file, log)
                                         
                     elif(x_split[0] == "RET"):
-                        # clean_op = x_split[0]
-                        # log.write("OPCODE : " + x_split[0] + '\n')
-                        # Data.current_addr_cnt = Data.next_addr_cnt
-                        # Data.addr_data.append(Data.addr_detail)
-                        # Data.next_addr_cnt += RETaddr.ret_addr
-                        # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                        
-                        # log.write("addr detail: \n")
-                        # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                        # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                        # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                        # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                        RET.ret_location(x_split, text_file, log)
+                        Location.ret_location(x_split, text_file, log)
                         RET.ret(x_split, text_file, log)
                         
 
                     elif x_split[0] == "SUBB":
                         if ("A" in x_split[1]) and ("@R" in x_split[2]):
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += SUBBaddr.A_Ri_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            SUBB.A_Ri_location(x_split, text_file, log)
+                            Location.SUBBA_Ri_location(x_split, text_file, log)
                             SUBB.A_Ri(x_split, text_file, log)
 
                         else:
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += SUBBaddr.A_Rn_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            SUBB.A_Rn_location(x_split, text_file, log)
+                            Location.SUBBA_Rn_location(x_split, text_file, log)
                             SUBB.A_Rn(x_split, text_file, log)
 
                     elif(x_split[0] == "XRL"):
                         if ("H" in x_split[1]) and ("#" in x_split[2]):
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += XRLaddr.direct_imm_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: ")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            XRL.direct_imm_location(x_split, text_file, log)
+                            Location.XRLdirect_imm_location(x_split, text_file, log)
                             XRL.direct_imm(x_split, text_file, log)
 
                         elif ("H" in x_split[1]) and ("A" in x_split[2]):
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += XRLaddr.direct_A_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            XRL.direct_A_location(x_split, text_file, log)                            
+                            Location.XRLdirect_A_location(x_split, text_file, log)                            
                             XRL.direct_A(x_split, text_file, log)
                     
                         elif ("A" in x_split[1]) and ("H" in x_split[2]):
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += XRLaddr.A_direct_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            XRL.A_direct_location(x_split, text_file, log)
+                            Location.XRLA_direct_location(x_split, text_file, log)
                             XRL.A_direct(x_split, text_file, log)
 
 
                     elif(x_split[0] == "LCALL"):
-                        # clean_op = x_split[0]
-                        # Data.current_addr_cnt = Data.next_addr_cnt
-                        # Data.addr_data.append(Data.addr_detail)
-                        # Data.next_addr_cnt += LCALLaddr.lcall_addr
-                        # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                        
-                        # log.write("addr detail: \n")
-                        # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                        # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                        # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                        # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                        LCALL.lcall_location(x_split, text_file, log)
+                        Location.lcall_location(x_split, text_file, log)
                         LCALL.lcall(x_split, text_file, log)
 
                     
                     elif(x_split[0] == "INC"):
                         if "@" in x_split[1]:
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += INCaddr.Ri_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            INC.Ri_location(x_split, text_file, log)
+                            Location.INCRi_location(x_split, text_file, log)
                             INC.Ri(x_split, text_file, log)
                         
                         else:
-                            # clean_op = x_split[0]
-                            # Data.current_addr_cnt = Data.next_addr_cnt
-                            # Data.addr_data.append(Data.addr_detail)
-                            # Data.next_addr_cnt += INCaddr.Rn_addr
-                            # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                            
-                            # log.write("addr detail: \n")
-                            # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                            # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                            # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                            # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                            INC.Rn_location(x_split, text_file, log)
+                            Location.INCRn_location(x_split, text_file, log)
                             INC.Rn(x_split, text_file, log)
 
 
                         
                     elif(x_split[0] == "JZ"):
-                        # clean_op = x_split[0]
-                        # Data.current_addr_cnt = Data.next_addr_cnt
-                        # Data.addr_data.append(Data.addr_detail)
-                        # Data.next_addr_cnt += JZaddr.jz_addr
-                        # Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
-                        
-                        # log.write("addr detail: \n")
-                        # log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
-                        # log.write("opcode: " + Data.addr_detail[1] + '\n')
-                        # log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
-                        # log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
-                        JZ.jz_location(x_split, text_file, log)
+                        Location.jz_location(x_split, text_file, log)
                         JZ.jz(x_split, text_file, log)
                     
                 
---
+++ location.py
@@ -0,0 +1,197 @@
+from util import tool
+from Address import Data, MOVaddr, RETaddr, SUBBaddr, XRLaddr, LCALLaddr, INCaddr, JZaddr
+
+output_file_path = "Test01-out.txt"
+text_file = "Test01.txt"
+log = "log.txt"
+
+class Location:
+    def label_location(x_split, text_file, log):
+        clean_op = tool.check_format(x_split[0])
+        log.write("LABEL: " + clean_op + '\n')
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += 0
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+        
+
+        log.write("-----------------------------\n")
+
+    def MOVdirect_direct_location(x_split, text_file, log):
+            clean_op = tool.check_format(x_split[0])
+            log.write("OPCODE : " + clean_op + '\n')
+            Data.current_addr_cnt = Data.next_addr_cnt
+            Data.addr_data.append(Data.addr_detail)
+            Data.next_addr_cnt += MOVaddr.direct_direct_addr
+            Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+            
+            log.write("addr detail: \n")
+            log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+            log.write("opcode: " + Data.addr_detail[1] + '\n')
+            log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+            log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def MOVreg_imm_location(x_split, text_file, log):
+        clean_op = tool.check_format(x_split[0])
+        log.write("OPCODE : " + clean_op + '\n')
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += MOVaddr.reg_imm_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def MOVRn_direct_location(x_split, text_file, log):
+        clean_op = tool.check_format(x_split[0])
+        log.write("OPCODE : " + clean_op + '\n')
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += MOVaddr.Rn_direct_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def SUBBA_Ri_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += SUBBaddr.A_Ri_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def SUBBA_Rn_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += SUBBaddr.A_Rn_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def XRLdirect_imm_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += XRLaddr.direct_imm_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: ")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def XRLdirect_A_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += XRLaddr.direct_A_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def XRLA_direct_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += XRLaddr.A_direct_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def lcall_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += LCALLaddr.lcall_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def INCRi_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += INCaddr.Ri_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def INCRn_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += INCaddr.Rn_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def jz_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += JZaddr.jz_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
+
+    def ret_location(x_split, text_file, log):
+        clean_op = x_split[0]
+        log.write("OPCODE : " + x_split[0] + '\n')
+        Data.current_addr_cnt = Data.next_addr_cnt
+        Data.addr_data.append(Data.addr_detail)
+        Data.next_addr_cnt += RETaddr.ret_addr
+        Data.addr_detail = [Data.addr_index, clean_op, Data.current_addr_cnt, Data.next_addr_cnt]
+        
+        log.write("addr detail: \n")
+        log.write("addr index: " + str(Data.addr_detail[0]) + '\n')
+        log.write("opcode: " + Data.addr_detail[1] + '\n')
+        log.write("current addr: " + str(Data.addr_detail[2]) + '\n')
+        log.write("next addr: " + str(Data.addr_detail[3]) + '\n')
